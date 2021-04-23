9124d450075cb9001daad7f1a77c15972ac0ce25
mwangiwanyeki <37798128+mwangiwanyeki@users.noreply.github.com>
2021-04-23 14:18:07 +0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-
-const User = mongoose.model("User", {
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -44,5 +43,9 @@ const User = mongoose.model("User", {
         },
     },
 });
+
+
+
+const User = mongoose.model("User", userSchema );
 
 module.exports = User;
